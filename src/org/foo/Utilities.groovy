418b3182af0a672ad47b0dcf83b151330caf9747
Harshitha-G <harshitha.g@intimetec.com>
2017-08-11 14:10:07 +0530

    Update Utilities.groovy

--- src/org/foo/Utilities.groovy
+++ src/org/foo/Utilities.groovy
@@ -3,6 +3,6 @@ class Utilities implements Serializable {
   def steps
   Utilities(steps) {this.steps = steps}
   def mvn(args) {
-    steps.sh "${steps.tool 'apache-maven-3.5.0'}/bin/mvn -o ${args}"
+    steps.bat "${steps.tool 'apache-maven-3.5.0'}/bin/mvn -o ${args}"
   }
 }
