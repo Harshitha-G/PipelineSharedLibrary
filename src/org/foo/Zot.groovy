b36129ab07105d8e14643aa1129921ac4451eb68
Harshitha-G <harshitha.g@intimetec.com>
2017-08-11 11:42:40 +0530

    Update Zot.groovy

--- src/org/foo/Zot.groovy
+++ src/org/foo/Zot.groovy
@@ -1,11 +1,6 @@
 // src/org/foo/Zot.groovy
 package org.foo;
 
-  /*def checkOutFrom(repo) {
+  def checkOutFrom(repo) {
     git url: "git@github.com:Harshitha-G/${repo}"
-  
-}*/
-def checkOutFrom(repo) {
-  git url: "git@github.com:jenkinsci/${repo}"
-}
-
+  }
