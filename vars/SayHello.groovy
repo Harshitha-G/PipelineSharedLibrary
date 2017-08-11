646f7eb840bc880e0ac87814115b521f26a57951
Harshitha-G <harshitha.g@intimetec.com>
2017-08-11 12:51:03 +0530

    Create SayHello.groovy

---
+++ vars/SayHello.groovy
@@ -0,0 +1,6 @@
+// vars/SayHello.groovy
+def call(String name = 'human') {
+    // Any valid steps can be called from this code, just like in other
+    // Scripted Pipeline
+    echo "Hello, ${name}."
+}
