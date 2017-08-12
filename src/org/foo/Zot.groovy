5a722ab12f92c506211fd40b6e2f5f626155af87
Harshitha-G <harshitha.g@intimetec.com>
2017-08-12 10:26:36 +0530

    Update Zot.groovy

--- src/org/foo/Zot.groovy
+++ src/org/foo/Zot.groovy
@@ -1,7 +1,6 @@
 // src/org/foo/Zot.groovy
 package org.foo;
-class Zot implements Serializable{
-  def checkOutFrom(repo) {
-    git url: "https://github.com/Harshitha-G/${repo}.git"
-  }
+def checkOutFrom(repo){
+  git url: "https://github.com/Harshitha-G/${repo}.git"
 }
+
