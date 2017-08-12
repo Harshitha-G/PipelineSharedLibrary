fbb131eb302657c369c6e779343ddb4955e3c028
Harshitha-G <harshitha.g@intimetec.com>
2017-08-12 10:45:36 +0530

    Update Utilities.groovy

--- src/org/foo/Utilities.groovy
+++ src/org/foo/Utilities.groovy
@@ -2,9 +2,6 @@ package org.foo
 class Utilities implements Serializable {
   def steps
   Utilities(steps) {this.steps = steps}
-  def checkOutFrom(){
-    steps.git "https://github.com/Harshitha-G/jenkins2-course-spring-petclinic.git"
-  }
   def mvn(args) {
     steps.bat "mvn ${args}"
   }
